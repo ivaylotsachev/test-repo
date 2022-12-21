c2c5c57810eed057f8509a7fe4181ec6a68ca6a6
ivaylotsachev <itsa4ev@gmail.com>
2022-12-21 16:15:37 +0200

    lenis as smooth scroll

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <template>
     <app-header />
-    <div class="page">
-        <router-view />
-    </div>
+    <router-view />
     <Cursor />
 </template>
 
 <script setup>
+import { initLenis } from './utils/lenis';
 // components
 import AppHeader from './components/Header.vue';
 import Cursor from './components/cursor/Cursor.vue';
+
+initLenis();
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,9 @@
 <template>
-    <div class="container flex flex-center">
-        <div class="titles-wrapper flex flex-center flex-column mt-5">
-            <h1 ref="titleOne" class="title title-one flex uppercase"></h1>
-            <h1 ref="titleTwo" class="title title-two flex uppercase"></h1>
-            <h1 ref="titleThree" class="title title-three flex uppercase"></h1>
+    <div class="relative container flex flex-center min-h-100-vh">
+        <div
+            class="titles-wrapper flex flex-center flex-column w-100-vw h-100-vh"
+        >
+            <h1 ref="titleOne" class="title title-one flex uppercase">Home</h1>
         </div>
     </div>
 </template>
-
-<script setup>
-/* imports */
-import { ref, onMounted } from 'vue';
-import { lettering } from '../utils/lettering';
-
-const titleOne = ref(null);
-const titleTwo = ref(null);
-const titleThree = ref(null);
-
-/* lifecycles */
-onMounted(() => {
-    lettering('creative', titleOne.value);
-    lettering('frotend', titleTwo.value);
-    lettering('developer', titleThree.value);
-});
-</script>
