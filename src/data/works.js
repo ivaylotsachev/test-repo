480d223c6266d604312d27564c589a3d9d5ec940
itsachev <itsachev@evolution.com>
2023-01-12 13:54:15 +0200

    works page

--- src/data/works.js
+++ src/data/works.js
@@ -1,11 +1,12 @@
 import boomburgersUrl from '../assets/boomburgers.jpg';
 import newbloomUrl from '../assets/newbloom.jpg';
+import redtiger from '../assets/redtiger.jpg';
 
 export default [
     {
         id: 'boomburgers',
         title: 'Boom Burgers',
-        subtitle: 'We make burgers. Gourmet burgers!',
+        subtitle: 'We make burgers. Gourmet burgers',
         role: 'Frontend Development',
         tags: ['UI/UX', 'Html', 'Css', 'Javascript'],
         url: '/works/boomburgers',
@@ -18,11 +19,22 @@ export default [
     {
         id: 'newbloomwinery',
         title: 'New Bloom Winery',
-        subtitle: 'Qaulity wines for special moment!',
+        subtitle: 'Qaulity wines for special moment',
         role: 'Frontend Development',
         tags: ['UI/UX', 'Html', 'Css', 'Javascript', 'Web Animations'],
         url: '/works/newbloomwinery',
         websiteUrl: 'https://www.roobar.com',
         imageUrl: newbloomUrl,
+    },
+
+    {
+        id: 'redtiger',
+        title: 'Red Tiger',
+        subtitle: 'Leading supplier of online casino software',
+        role: 'Frontend Development',
+        tags: ['UI/UX', 'Html', 'Css', 'Javascript', 'Web Animations'],
+        url: '/works/newbloomwinery',
+        websiteUrl: 'https://www.redtiger.com',
+        imageUrl: redtiger,
     },
 ];
--- src/views/works/WorksView.vue
+++ src/views/works/WorksView.vue
@@ -1,24 +1,58 @@
 <template>
-    <div class="page works-page p-5">
-        <section
-            class="flex flex-column flex-center inner-container w-100 h-100"
-        >
-            <div class="works-images-container" ref="imagesContainer">
+    <div class="page works-page flex flex-center p-5">
+        <section class="flex flex-center">
+            <div class="works-titles-container">
+                <div
+                    class="work-title-item magnetic"
+                    :class="'item-' + item.id"
+                    v-for="(item, index) in worksdata"
+                    :key="item.id"
+                    :item="item"
+                    :data-name="item.id"
+                    :itemIndex="index"
+                    data-strength="30"
+                    data-strength-text="10"
+                    @mouseenter="handleTitleMouseEnter(item.id, $event)"
+                    @mouseleave="handleTitleMouseLeave"
+                    @mousemove="handleTitleMouseMove"
+                >
+                    <div class="title-container flex flex-center jcfs">
+                        <img
+                            v-if="store.isMobile"
+                            :src="item.imageUrl"
+                            alt=""
+                        />
+                        <div class="mask order-mask">
+                            <p class="mr-3 item-order">#{{ index + 1 }}</p>
+                        </div>
+                        <div>
+                            <h2 class="title title-grotesk">
+                                {{ item.title }}
+                            </h2>
+                            <div class="mask">
+                                <p class="item-subtitle">{{ item.subtitle }}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div
+                        v-if="index < worksdata.length - 1"
+                        class="separator"
+                    ></div>
+                </div>
+            </div>
+
+            <div
+                v-if="!store.isMobile"
+                class="works-images-container magnetic"
+                ref="imagesContainer"
+            >
                 <img
                     v-for="item in worksdata"
-                    class="works-image w-100 h-100"
+                    ref="images"
                     :src="item.imageUrl"
                     :alt="item.title"
                     :key="item.id"
-                />
-            </div>
-            <div
-                class="works-items-container flex flex-center flex-column aifs"
-            >
-                <WorksListItem
-                    v-for="item in worksdata"
-                    :key="item.id"
-                    :item="item"
+                    :data-name="item.id"
                 />
             </div>
         </section>
@@ -26,19 +60,44 @@
 </template>
 
 <script setup>
-import { onMounted, watch, onBeforeUnmount, nextTick } from 'vue';
+import { ref, onMounted, watch, onBeforeUnmount, nextTick } from 'vue';
 import { useAppStore } from '../../stores/app';
 import { useRouter } from 'vue-router';
-import WorksListItem from '../../components/works-list-item/WorksListItem.vue';
+import gsap from 'gsap';
 /* utils */
 import initLenis from '../../utils/lenis';
 import worksdata from '../../data/works';
+import magnetics from '../../utils/magnetics';
+import gsapCore from 'gsap/gsap-core';
 
 const store = useAppStore();
 const router = useRouter();
+const images = ref(null);
+const imagesContainer = ref(null);
 let lenis;
 
 store.setActivePage('works');
+
+/* methods */
+const handleTitleMouseMove = (event) => magnetics.magnetIn(event);
+const handleTitleMouseLeave = (event) => {
+    store.setCursor('');
+    magnetics.magnetOut(event);
+};
+const handleTitleMouseEnter = (name, event) => {
+    store.setCursor('project');
+    const dataname = event.target.getAttribute('data-name');
+
+    images.value.forEach((image) => {
+        const imageDataname = image.getAttribute('data-name');
+
+        if (dataname === imageDataname) {
+            gsap.fromTo(image, { scale: 1.2 }, { scale: 1, autoAlpha: 1 });
+        } else {
+            gsap.to(image, { autoAlpha: 0 });
+        }
+    });
+};
 
 watch(
     () => store.router,
@@ -52,6 +111,19 @@ watch(
     }
 );
 
+const animateImages = (e) => {
+    // gsap.to(imagesContainer.value, { autoAlpha: 0.6, duration: 1 });
+    // const containerRect = imagesContainer.value.getBoundingClientRect();
+    // const left = e.pageX - containerRect.width / 2 + 'px';
+    // const top = e.pageY - containerRect.height / 2 + 'px';
+    // gsap.to(imagesContainer.value, {
+    //     left,
+    //     top,
+    //     duration: 0.4,
+    //     ease: 'Power3.easeOut',
+    // });
+};
+
 onMounted(async () => {
     await nextTick();
 
@@ -63,11 +135,16 @@ onMounted(async () => {
     if (from === 'home') {
         console.log('works: animate from home here');
     }
+
+    window.addEventListener('mousemove', animateImages);
 });
 
-onBeforeUnmount(() => lenis.destroy());
+onBeforeUnmount(() => {
+    lenis.destroy();
+    window.removeEventListener('mousemove', animateImages);
+});
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 @import './WorksView.scss';
 </style>
