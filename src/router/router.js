9665fd397cb736a30023b995bc12742bf9c7f28b
ivaylo tsachev <itsa4ev@gmail.com>
2023-01-14 20:59:15 +0200

    work route

--- src/router/router.js
+++ src/router/router.js
@@ -1,12 +1,16 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
-import HomeView from '../views/home/HomeView.vue';
-import WorksView from '../views/works/WorksView.vue';
-import WorkView from '../views/work/WorkView.vue';
+import { createRouter, createWebHashHistory } from "vue-router";
+import HomeView from "../views/home/HomeView.vue";
+import WorksView from "../views/works/WorksView.vue";
+import WorkView from "../views/work/WorkView.vue";
 
 const routes = [
-    { path: '/', name: 'home', component: HomeView },
-    { path: '/works', name: 'works', component: WorksView },
-    { path: '/works/:id', name: 'work', component: WorkView },
+    { path: "/", name: "home", component: HomeView },
+    {
+        path: "/works",
+        name: "works",
+        component: WorksView,
+    },
+    { path: "/:id", name: "work", component: WorkView },
 ];
 
 export default createRouter({
